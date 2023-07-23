21ba53aea5b57b54882903e875b39bd94ab4cc69
jybin96 <jybin96@changwon.ac.kr>
2023-07-23 17:46:57 +0900

    Feat : jenkins 무중단 자동 배포

--- BackEnd/src/main/java/com/example/backend/security/SecurityConfig.java
+++ BackEnd/src/main/java/com/example/backend/security/SecurityConfig.java
@@ -79,6 +79,8 @@ public class SecurityConfig {
         skipPathList.add(new Path(HttpMethod.POST, "/user"));
         skipPathList.add(new Path(HttpMethod.GET, "/docs/**"));
         skipPathList.add(new Path(HttpMethod.GET, "/v3/**"));
+        skipPathList.add(new Path(HttpMethod.GET, "/profile"));
+        skipPathList.add(new Path(HttpMethod.GET, "/actuator/health"));
         skipPathList.add(new Path(HttpMethod.POST, "/auth/nickname"));
         skipPathList.add(new Path(HttpMethod.POST, "/auth/email"));
         skipPathList.add(new Path(HttpMethod.POST, "/auth/code"));
