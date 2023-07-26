4871d2690821809aa438a61a7e07b012f322b964
김민구 <tkwkalsrn@naver.com>
2023-07-26 14:43:35 +0900

    Feat : 비밀번호 변경 API 작성
    - 입력한 비밀번호로 해당 유저의 비밀번호를 변경
    - DB의 비밀번호 변경 확인
    - 변경된 비밀번호로 로그인 확인

---
+++ BackEnd/src/test/java/com/example/backend/controller/LoginRequestDto.java
@@ -0,0 +1,28 @@
+package com.example.backend.controller;
+
+public class LoginRequestDto {
+
+    private String email;
+    private String password;
+
+    public LoginRequestDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
