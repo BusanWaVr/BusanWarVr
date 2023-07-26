046d7e2c1e57954d104319a27fe1f664e8e94b70
jh2dang <bluemin_@naver.com>
2023-07-27 00:22:52 +0900

    Feat : 채팅룸 채팅 표시 채팅룸 채팅 표시

--- FrontEnd/src/routes/ChatRoom.tsx
+++ FrontEnd/src/routes/ChatRoom.tsx
@@ -1,6 +1,6 @@
+import React, { useEffect, useState } from "react";
 import SockJS from "sockjs-client/dist/sockjs";
 import Stomp from "stompjs";
-import { useEffect } from "react";
 
 let sockJS = new SockJS("http://13.209.65.4/ws-stomp");
 let stompClient = Stomp.over(sockJS);
@@ -11,11 +11,19 @@ export type message = {
 };
 
 function ChatRoom() {
+  const [chatMessages, setChatMessages] = useState<message[]>([]); // Initialize chat messages state
+  const [inputMessage, setInputMessage] = useState("");
+
   useEffect(() => {
     stompClient.connect({}, () => {
       console.log("연결됨");
       stompClient.subscribe("/sub/chat/message/room/1", (data) => {
-        console.log(JSON.parse(data.body));
+        const receivedMessage = JSON.parse(data.body);
+        const newChatMessage = {
+          username: receivedMessage.sender.nickname,
+          content: receivedMessage.message,
+        };
+        setChatMessages((prevMessages) => [...prevMessages, newChatMessage]);
       });
     });
   }, []);
@@ -25,9 +33,12 @@ function ChatRoom() {
       roomId: 1,
       token:
         "BEARER eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFWFBJUkVEX0RBVEUiOjE2OTAzOTMzOTEsImlzcyI6InRlc3QiLCJVU0VSX05BTUUiOiJzb2NrZXN0MTJAdGVzdC5jb20ifQ.LRVLb35pIKQfgq-w0TKVnCqTvMZdMZJkdqY7KXLZDyY",
-      message: "asdasdas",
+      message: inputMessage,
     };
     stompClient.send("/pub/chat/message", {}, JSON.stringify(newMessage));
+    console.log(chatMessages);
+    // console.log(inputMessage);
+    setInputMessage("");
   };
 
   const handleEnter2 = () => {
@@ -38,13 +49,27 @@ function ChatRoom() {
       message: "하이",
     };
     stompClient.send("/pub/chat/message", {}, JSON.stringify(newMessage));
+    console.log(chatMessages);
   };
 
   return (
     <>
       <h1>ChatRoom 페이지입니다.</h1>
-      <button onClick={handleEnter}>내가 메시지 보내기</button>
-      <button onClick={handleEnter2}>상대방이 메시지 보내기</button>
+      <div>
+        {chatMessages.map((msg, index) => (
+          <div key={index}>
+            <strong>{msg.username}</strong>: {msg.content}
+          </div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={inputMessage}
+        onChange={(e) => setInputMessage(e.target.value)}
+      />
+      <button onClick={handleEnter}>send</button>
+      <br />
+      <button onClick={handleEnter2}>상대방이 메시지 보내기 test</button>
     </>
   );
 }
