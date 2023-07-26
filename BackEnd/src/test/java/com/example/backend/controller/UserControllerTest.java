d4df6089e1a87e5168b031b191830a9554711c2c
jaeyoung <wodud9515@naver.com>
2023-07-26 10:03:30 +0900

    Fix : 카테고리 3개 이하 회원가입 성공하는 경우 수정 #4
    - 카테고리가 3개 이하인데 회원가입이 되는 경우를 불가능하게 수정

--- BackEnd/src/test/java/com/example/backend/controller/UserControllerTest.java
+++ BackEnd/src/test/java/com/example/backend/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.example.backend.controller;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import com.example.backend.dto.Response;
 import com.example.backend.exception.ErrorResponse;
 import com.example.backend.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -35,44 +36,27 @@ class UserControllerTest {
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/user")
                         .param("email", "test1@naver.com")
                         .param("password", "test12345!")
-                        .param("nickname", "jae"))
-                .andReturn();
-
-        // 회원가입이 성공적으로 이루어졌는지 확인
-        assertThat(mvcResult.getResponse().getStatus()).isEqualTo(200);
-
-    }
-
-    @Test
-    @DisplayName("사용자 회원가입 실패 테스트 - 비밀번호 ")
-    void signUpFailTestPassword() throws Exception {
-
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/user")
-                        .param("email", "test1@naver.com")
-                        .param("password", "test12345")
-                        .param("nickname", "jae"))
+                        .param("nickname", "jae")
+                        .param("category", "힐링, 여행, 체험"))
                 .andReturn();
 
         String content = mvcResult.getResponse().getContentAsString();
-
         objectMapper = new ObjectMapper();
-        ErrorResponse response = objectMapper.readValue(content, ErrorResponse.class);
-        // status 400
-        assertThat(response.getStatus()).isEqualTo("400");
-        // FieldError 안의 field가 password가 존재하는지
-        assertThat(response.getErrors().get(0).getField()).isEqualTo("password");
-        // 비밀번호 길이, 유효성 모두 걸렸기때문에 errors의 길이가 2인지.
-        assertThat(response.getErrors().size()).isEqualTo(2);
+        Response response = objectMapper.readValue(content, Response.class);
+
+        assertThat(response.getCode()).isEqualTo("200");
+
     }
 
     @Test
-    @DisplayName("사용자 회원가입 실패 테스트 - 닉네임")
-    void signUpFailTestNickname() throws Exception {
+    @DisplayName("사용자 회원가입 실패 테스트 - 닉네임(숫자,한글,영어 외 포함)")
+    void signUpFailTestNickname1() throws Exception {
 
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/user")
                         .param("email", "test2@naver.com")
                         .param("password", "test12345!")
-                        .param("nickname", "!"))
+                        .param("nickname", "!!")
+                        .param("category", "힐링, 여행, 체험"))
                 .andReturn();
 
         String content = mvcResult.getResponse().getContentAsString();
@@ -82,8 +66,89 @@ class UserControllerTest {
 
         assertThat(response.getStatus()).isEqualTo("400");
         assertThat(response.getErrors().get(0).getField()).isEqualTo("nickname");
-        // 닉네임의 유효성, 길이 걸렸기때문에 errors의 길이가 2
-        assertThat(response.getErrors().size()).isEqualTo(2);
+        assertThat(response.getErrors().size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("사용자 회원가입 실패 테스트 - 닉네임(길이 부족)")
+    void signUpFailTestNickname2() throws Exception {
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/user")
+                        .param("email", "test2@naver.com")
+                        .param("password", "test12345!")
+                        .param("nickname", "f")
+                        .param("category", "힐링, 여행, 체험"))
+                .andReturn();
+
+        String content = mvcResult.getResponse().getContentAsString();
+
+        objectMapper = new ObjectMapper();
+        ErrorResponse response = objectMapper.readValue(content, ErrorResponse.class);
+
+        assertThat(response.getStatus()).isEqualTo("400");
+        assertThat(response.getErrors().get(0).getField()).isEqualTo("nickname");
+        assertThat(response.getErrors().size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("사용자 회원가입 실패 테스트 - 닉네임(길이 초과)")
+    void signUpFailTestNickname3() throws Exception {
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/user")
+                        .param("email", "test2@naver.com")
+                        .param("password", "test12345!")
+                        .param("nickname", "fdddddddddddddddddddddd")
+                        .param("category", "힐링, 여행, 체험"))
+                .andReturn();
+
+        String content = mvcResult.getResponse().getContentAsString();
+
+        objectMapper = new ObjectMapper();
+        ErrorResponse response = objectMapper.readValue(content, ErrorResponse.class);
+
+        assertThat(response.getStatus()).isEqualTo("400");
+        assertThat(response.getErrors().get(0).getField()).isEqualTo("nickname");
+        assertThat(response.getErrors().size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("사용자 회원가입 실패 테스트 - 비밀번호(영문자,숫자,특수문자 중 하나 미포함) ")
+    void signUpFailTestPassword1() throws Exception {
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/user")
+                        .param("email", "test1@naver.com")
+                        .param("password", "test123456")
+                        .param("nickname", "jae")
+                        .param("category", "힐링, 여행, 체험"))
+                .andReturn();
+
+        String content = mvcResult.getResponse().getContentAsString();
+
+        objectMapper = new ObjectMapper();
+        ErrorResponse response = objectMapper.readValue(content, ErrorResponse.class);
+        assertThat(response.getStatus()).isEqualTo("400");
+        assertThat(response.getErrors().get(0).getField()).isEqualTo("password");
+        assertThat(response.getErrors().size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("사용자 회원가입 실패 테스트 - 비밀번호(길이 부족) ")
+    void signUpFailTestPassword2() throws Exception {
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/user")
+                        .param("email", "test1@naver.com")
+                        .param("password", "abc12345!")
+                        .param("nickname", "jae")
+                        .param("category", "힐링, 여행, 체험"))
+                .andReturn();
+
+        String content = mvcResult.getResponse().getContentAsString();
+
+        objectMapper = new ObjectMapper();
+        ErrorResponse response = objectMapper.readValue(content, ErrorResponse.class);
+        assertThat(response.getStatus()).isEqualTo("400");
+        assertThat(response.getErrors().get(0).getField()).isEqualTo("password");
+        assertThat(response.getErrors().size()).isEqualTo(1);
     }
 
     @Test
@@ -93,7 +158,8 @@ class UserControllerTest {
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/user")
                         .param("email", "test1@") // 이메일 형식 벗어남
                         .param("password", "test12345!")
-                        .param("nickname", "jae2"))
+                        .param("nickname", "jae2")
+                        .param("category", "힐링, 여행, 체험"))
                 .andReturn();
 
         String content = mvcResult.getResponse().getContentAsString();
@@ -103,7 +169,6 @@ class UserControllerTest {
 
         assertThat(response.getStatus()).isEqualTo("400");
         assertThat(response.getErrors().get(0).getField()).isEqualTo("email");
-        // 이메일의 형식만 걸렸기때문에 errors의 길이가 1
         assertThat(response.getErrors().size()).isEqualTo(1);
     }
 }
