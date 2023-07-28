1f8afa1ff74767a543e836eecfff937f878c561c
niyamallo <treenicole@naver.com>
2023-07-28 13:11:35 +0900

    Feat : Live QR 동적으로 동작하도록 로직 변경

--- FrontEnd/src/components/QRCodeComponent.tsx
+++ FrontEnd/src/components/QRCodeComponent.tsx
@@ -1,21 +1,32 @@
 import React, { useEffect, useState } from "react";
 import QRCode from "qrcode.react";
+import styled from "styled-components";
 
-const QRCodeComponent: React.FC = () => {
+const QRcodeDiv = styled.div`
+  position: fixed;
+  top: 100px;
+  right: 30px;
+  width: 200px;
+`;
+
+interface QRProps {
+  youtubeLink: string;
+}
+
+function QRCodeComponent({ youtubeLink }: QRProps) {
   const [url, setUrl] = useState<string>("");
 
   useEffect(() => {
     // 컴포넌트가 마운트될 때 페이지의 URL을 가져옵니다.
-    const currentUrl = window.location.href;
-    setUrl(currentUrl);
+    setUrl(youtubeLink);
   }, []);
 
   return (
-    <div>
+    <QRcodeDiv>
       {/* QR 코드 컴포넌트에 현재 페이지의 URL을 전달하여 QR 코드를 생성합니다. */}
-      {url && <QRCode value={url} size={256} />}
-    </div>
+      {url && <QRCode value={url} size={100} />}
+    </QRcodeDiv>
   );
-};
+}
 
 export default QRCodeComponent;
