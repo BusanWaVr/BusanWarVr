08bfe3951a9c993b0eaa20c1f9d3b40a125c4377
jaeyoung <wodud9515@naver.com>
2023-07-28 13:59:33 +0900

    Feat : CourseDto 수정

--- BackEnd/src/main/java/com/example/backend/dto/CourseDto.java
+++ BackEnd/src/main/java/com/example/backend/dto/CourseDto.java
@@ -15,4 +15,14 @@ public class CourseDto {
         private Long tourId;
         private MultipartFile image;
     }
+
+    @Data
+    public static class Response{
+        private double lon;
+        private double lat;
+        private String title;
+        private String content;
+        private Long tourId;
+        private String image;
+    }
 }
