c16320c750da11612bb20e2633144cb3456f0c5b
jh2dang <bluemin_@naver.com>
2023-08-01 00:26:43 +0900

    Feat : 메이트 목록 페이지네이션 메이트 목록 페이지네이션

--- FrontEnd/src/components/pages/Mate/MateList.jsx
+++ FrontEnd/src/components/pages/Mate/MateList.jsx
@@ -2,45 +2,72 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const MateList = () => {
-    const [ mateListData, setMateListData ] = useState(null);
-  
-    useEffect(() => {
-      const fetchMateListData = async () => {
-        try {
-          const response = await fetch("http://52.79.93.203/mate");
-          if (response.status === 200) {
-            const data = await response.json();
-            setMateListData(data.data.mateList);
-  
-          } else {
-            // 아마도...
-            alert("메이트 목록을 불러올 수 없습니다. 잠시 후 다시 시도해 주세요.")
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
-  
-      fetchMateListData();
-    }, []);
-  
-    return (
-      <div>
-        <h1>메이트 목록 페이지</h1>
-        {mateListData && mateListData.map((mate) => (
-        <div key={mate.mateId}>
-          <h3><Link to={`/matedetail/${mate.mateId}`} style={{ fontWeight: "bold" }}>{mate.title}</Link></h3>
-          <div dangerouslySetInnerHTML={{ __html: mate.content }} />
-          <p>참가인원: {mate.joinMember}/{mate.maxMember}</p>
+  const [mateListData, setMateListData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(1);
 
-          {/* /tour/{mate.tourId}로 이동할 수 있는 링크 추가할까말까 */}
-          {/* <a href={`/tour/${mate.tourId}`}>상세 보기</a> */}
-          <br />
-          <br />
-        </div>
-      ))}
-      </div>
-    );
+  useEffect(() => {
+    const fetchMateListData = async () => {
+      try {
+        const response = await fetch(`http://52.79.93.203/mate?page=${currentPage}`);
+        if (response.status === 200) {
+          const data = await response.json();
+          setMateListData(data.data.mateList);
+          
+
+          // 페이지 수
+          setTotalPages(Math.floor(data.data.totalCount / 6) + 1);
+        } else {
+          alert("메이트 목록을 불러올 수 없습니다. 잠시 후 다시 시도해 주세요.");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchMateListData();
+  }, [currentPage]);
+
+  const handlePrevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
   };
-  
-  export default MateList;
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages - 1) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  return (
+    <div>
+      <h1>메이트 목록 페이지</h1>
+      {mateListData.map((mate, index) => (
+        <div key={mate.mateId}>
+          <h3>
+            <Link to={`/matedetail/${mate.mateId}`} style={{ fontWeight: "bold" }}>
+              {mate.title}
+            </Link>
+          </h3>
+          <div dangerouslySetInnerHTML={{ __html: mate.content }} />
+          <p>
+            참가인원: {mate.joinMember}/{mate.maxMember}
+          </p>
+          {index % 3 === 2 && <br />}
+        </div>
+        
+      ))}
+      <div>
+        <button onClick={handlePrevPage} disabled={currentPage === 0}>
+          이전 페이지
+        </button>
+        <button onClick={handleNextPage} disabled={currentPage === totalPages - 1}>
+          다음 페이지
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MateList;
