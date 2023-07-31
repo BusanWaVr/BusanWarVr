215c8dd4c0d9ee651a2433fdc321e59650f4619d
황재영 <wodud9515@naver.com>
2023-08-01 00:11:14 +0900

    Update TourController.java

--- BackEnd/src/main/java/com/example/backend/controller/TourController.java
+++ BackEnd/src/main/java/com/example/backend/controller/TourController.java
@@ -60,6 +60,6 @@ public class TourController {
     public Response tourCancelApi(@PathVariable Long tourId, @AuthenticationPrincipal UserDetailsImpl userDetails)
             throws IllegalAccessException {
         tourService.tourCancel(tourId, userDetails.getUser());
-        return new Response("200", "성공적으로 투어 예약을 취소 하였습니다!", null);
+        return new Response("200", "성공적으로 투어를 취소 하였습니다!", null);
     }
 }
