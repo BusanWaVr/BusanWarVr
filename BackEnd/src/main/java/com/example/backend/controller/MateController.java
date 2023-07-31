51eb192821b1c64a4f9b1163f193f1b8bfd7a9c3
jybin96 <jybin96@changwon.ac.kr>
2023-07-31 20:27:57 +0900

    Feat : 메이트 모집 수정 API 추가 - 사용자와 해당 메이트를 비교하여 ID가 같지 않을 경우 Exception - 메이트 변경 사항이 없다면 Exception - title, content 변경 가능

--- BackEnd/src/main/java/com/example/backend/controller/MateController.java
+++ BackEnd/src/main/java/com/example/backend/controller/MateController.java
@@ -3,6 +3,7 @@ package com.example.backend.controller;
 import com.example.backend.dto.MateDetailDto;
 import com.example.backend.dto.MateListDto;
 import com.example.backend.dto.MateRegistDto;
+import com.example.backend.dto.MateUpdateDto;
 import com.example.backend.dto.Response;
 import com.example.backend.model.user.User;
 import com.example.backend.security.UserDetailsImpl;
@@ -14,6 +15,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +26,8 @@ public class MateController {
     private final MateService mateService;
 
     @PostMapping("/mate")
-    public Response<MateRegistDto.Response> registMate(@AuthenticationPrincipal UserDetailsImpl userDetails,
+    public Response<MateRegistDto.Response> registMate(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
             @RequestBody MateRegistDto.Request request) {
         User user = userDetails.getUser();
 
@@ -32,7 +35,8 @@ public class MateController {
             throw new IllegalArgumentException("가이드는 mate 등록을 할 수 없습니다.");
         }
 
-        MateRegistDto.Response response = mateService.registMateService(request, userDetails.getUser());
+        MateRegistDto.Response response = mateService.registMateService(request,
+                userDetails.getUser());
         return new Response<>("200", "성공적으로 메이트 등록 되었습니다!", response);
     }
 
@@ -47,5 +51,12 @@ public class MateController {
             @PageableDefault(size = 6) Pageable pageable) {
         MateListDto.Response response = mateService.getMateList(pageable);
         return new Response<>("200", "성곡적으로 메이트 리스트를 불러왔습니다.", response);
+    }
+
+    @PutMapping("/mate/{mateId}")
+    public Response putMate(@AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable Long mateId, @RequestBody MateUpdateDto.Request request) {
+        mateService.updateMate(request, mateId, userDetails.getUser());
+        return new Response<>("200", "성곡적으로 메이트 정보를 변경하였습니다.", null);
     }
 }
