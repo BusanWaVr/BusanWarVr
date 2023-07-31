5e9a8894f9c4daa4252be9f940bda3ad96c0a8d2
jh2dang <bluemin_@naver.com>
2023-07-31 21:17:56 +0900

    Feat : 메이트 상세페이지 임시 메이트 상세페이지 임시

--- FrontEnd/src/components/pages/Mate/MateDetail.jsx
+++ FrontEnd/src/components/pages/Mate/MateDetail.jsx
@@ -1,5 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const MateDetail = () => {
-  return <h1>메이트 상세페이지</h1>;
+  const { mateId } = useParams();
+  const [mateData, setMateData] = useState(null);
+  const [joinerData, setJoinerData] = useState(null);
+
+  useEffect(() => {
+    const fetchMateData = async () => {
+      try {
+        const response = await fetch(`http://52.79.93.203/mate/${mateId}`);
+        // console.log(response)
+        if (response.status === 200) {
+          const data = await response.json();
+  
+          // mate정보만 저장하기
+          setMateData(data.data.mate);
+
+          // joiner정보
+          setJoinerData(data.data.joiner);
+
+        } else {
+          // 아마도...
+          alert("해당 메이트 게시글이 존재하지 않습니다.")
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchMateData();
+  }, [mateId]);
+
+  return (
+    <div>
+      <h1>메이트 상세페이지</h1>
+      <br />
+      {mateData ? (
+        <div>
+          <h3><strong>{mateData.title}</strong></h3>
+          <br />
+          <div dangerouslySetInnerHTML={{ __html: mateData.content }} />
+        </div>
+      ) : (
+        <p>로딩중ㅎ</p>
+      )}
+    </div>
+  );
 };
 
 export default MateDetail;
