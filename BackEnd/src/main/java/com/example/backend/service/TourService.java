31cc7daf3967f6fcc7d68568a61a79211c7fec36
jaeyoung <wodud9515@naver.com>
2023-08-01 11:42:02 +0900

    Fix : Tour 작성 API 수정 및 Tour Entity 변경 요청
    - 프런트에서 WebRTC로 화상을 할때 고유한 sessionID가 필요합니다.
    - String 형태의 uid라는 칼럼을만들고 자바의 내부함수인 UUID를 이용하여 고유값을 만들고 거기다가 + "/date"를 붙혀서 저장

--- BackEnd/src/main/java/com/example/backend/service/TourService.java
+++ BackEnd/src/main/java/com/example/backend/service/TourService.java
@@ -53,57 +53,57 @@ public class TourService {
     public void tourRegist(TourRegistDto.Request request, User user)
             throws IOException, IllegalAccessException {
 
-        if (user.getType() == AuthType.GUIDE) {
-            Tour tour = request.toTour(user);
-            tourRepository.save(tour);
+        if (user.getType() != AuthType.GUIDE) {
+            throw new IllegalAccessException("가이드만 투어 등록이 가능합니다.");
+        }
+
+        Tour tour = request.toTour(user);
+        tourRepository.save(tour);
+
+        // 이미지 등록 부분 조건적으로 처리
+        if (request.getTourImgs() != null && !request.getTourImgs().isEmpty()) {
+            // 투어 이미지 저장해 url 가져와서 Image 객체 생성 및 저장
+            List<Image> tourImages = new ArrayList<>();
+            for (MultipartFile file : request.getTourImgs()) {
+                Image image = saveImage(file);
+                tourImages.add(image);
+            }
+
+            // TourImage 객체 생성 및 저장
+            for (Image image : tourImages) {
+                TourImage tourImage = new TourImage();
+                tourImage.setTour(tour);
+                tourImage.setImage(image);
+                tourImageRepository.save(tourImage);
+            }
+        }
+
+        // 가이드가 선택한 카테고리 이름들을 TourCategory 객체로 변환하고 저장
+        for (String categoryName : request.getCategory()) {
+            if (categoryRepository.findByName(categoryName) != null) {
+                tourCategoryCreate(tour, categoryRepository.findByName(categoryName));
+            } else {
+                throw new IllegalAccessException("등록된 카테고리만 추가 가능합니다.");
+            }
+        }
+
+        // Course 객체 생성 및 저장
+        for (CourseDto.Request courseDto : request.getCourses()) {
+            Course course = new Course(courseDto.getLon(), courseDto.getLat(),
+                    courseDto.getTitle(),
+                    courseDto.getContent(), tour.getId());
+            courseRepository.save(course);
 
             // 이미지 등록 부분 조건적으로 처리
-            if (request.getTourImgs() != null && !request.getTourImgs().isEmpty()) {
-                // 투어 이미지 저장해 url 가져와서 Image 객체 생성 및 저장
-                List<Image> tourImages = new ArrayList<>();
-                for (MultipartFile file : request.getTourImgs()) {
-                    Image image = saveImage(file);
-                    tourImages.add(image);
-                }
+            if (courseDto.getImage() != null ) {
+                Image image = saveImage(courseDto.getImage());
 
-                // TourImage 객체 생성 및 저장
-                for (Image image : tourImages) {
-                    TourImage tourImage = new TourImage();
-                    tourImage.setTour(tour);
-                    tourImage.setImage(image);
-                    tourImageRepository.save(tourImage);
-                }
+                // CourseImage 객체 생성 및 저장
+                CourseImage courseImage = new CourseImage();
+                courseImage.setCourse(course);
+                courseImage.setImage(image);
+                courseImageRepository.save(courseImage);
             }
-
-            // 가이드가 선택한 카테고리 이름들을 TourCategory 객체로 변환하고 저장
-            for (String categoryName : request.getCategory()) {
-                if (categoryRepository.findByName(categoryName) != null) {
-                    tourCategoryCreate(tour, categoryRepository.findByName(categoryName));
-                } else {
-                    throw new IllegalAccessException("등록된 카테고리만 추가 가능합니다.");
-                }
-            }
-
-            // Course 객체 생성 및 저장
-            for (CourseDto.Request courseDto : request.getCourses()) {
-                Course course = new Course(courseDto.getLon(), courseDto.getLat(),
-                        courseDto.getTitle(),
-                        courseDto.getContent(), tour.getId());
-                courseRepository.save(course);
-
-                // 이미지 등록 부분 조건적으로 처리
-                if (courseDto.getImage() != null) {
-                    Image image = saveImage(courseDto.getImage());
-
-                    // CourseImage 객체 생성 및 저장
-                    CourseImage courseImage = new CourseImage();
-                    courseImage.setCourse(course);
-                    courseImage.setImage(image);
-                    courseImageRepository.save(courseImage);
-                }
-            }
-        } else {
-            throw new IllegalAccessException("가이드만 투어 등록이 가능합니다.");
         }
     }
 
