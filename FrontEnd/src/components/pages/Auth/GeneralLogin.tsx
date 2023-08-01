40e6acdf3a805bbd35bb497eb433254290d8c502
정은경 <f1rstf1y9@naver.com>
2023-08-01 23:03:54 +0900

    Feat : 유저 상태에 따른 라우팅 수정
    - Private : 로그인한 유저만
    - Public : 로그인 안 한 유저만
    - User : 일반 사용자만
    - Guide : 가이드 사용자만
    - [GitLab](https://lab.ssafy.com/s09-webmobile1-sub2/S09P12E102/-/issues/46)

--- FrontEnd/src/components/pages/Auth/GeneralLogin.tsx
+++ FrontEnd/src/components/pages/Auth/GeneralLogin.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import {
   setUserId,
   setEmail,
+  setUserType,
   changeNickname,
   changeProfileImg,
   changeAccessToken,
