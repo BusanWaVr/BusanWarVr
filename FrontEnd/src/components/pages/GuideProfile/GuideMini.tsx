29f4f286e63cd403f9fd71626cf9ab63ad11025f
niyamallo <treenicole@naver.com>
2023-08-01 09:29:10 +0900

    Fix : 가이드 미니프로필에서 메뉴바 분리 가이드 미니프로필에서 메뉴바 분리

--- FrontEnd/src/components/pages/GuideProfile/GuideMini.tsx
+++ FrontEnd/src/components/pages/GuideProfile/GuideMini.tsx
@@ -1,6 +1,7 @@
+// GuideMini.tsx
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { GuideData } from "./types";
+import GuideDetailMenu from "./GuideDetailMenu";
 import "./GuideMini.css";
 
 interface GuideMiniProps {
@@ -11,27 +12,21 @@ interface GuideMiniProps {
 const GuideMini: React.FC<GuideMiniProps> = ({ guide, onFollowClick }) => {
   const [isFollowing, setIsFollowing] = useState<boolean>(false);
 
-  const handleFollowClick = () => {
-    onFollowClick(); // Call the onFollowClick function passed from GuideDetail
-  };
-
   useEffect(() => {
-    console.log(guide.isFollowing);
     setIsFollowing(guide.isFollowing);
-  }, []);
+  }, [guide.isFollowing]);
+
+  const handleFollowClick = () => {
+    onFollowClick(); // GuideDetail에서 전달받은 onFollowClick 함수 호출
+  };
 
   const handleFollowClick2 = () => {
     // 팔로잉 취소 HTTP 통신
-    if (isFollowing) {
-      setIsFollowing(false);
-    } else {
-      setIsFollowing(true);
-    }
-    console.log(isFollowing);
+    setIsFollowing((prevIsFollowing) => !prevIsFollowing);
   };
 
   const handleHomeClick = () => {
-    window.scrollTo(0, 0); // Scroll to the top of the page
+    window.scrollTo(0, 0); // 페이지 맨 위로 스크롤
   };
 
   return (
@@ -43,11 +38,10 @@ const GuideMini: React.FC<GuideMiniProps> = ({ guide, onFollowClick }) => {
       </button>
       <p>팔로워 {guide.followers}</p>
       <p>투어 {guide.toursCount}</p>
-
-      <button onClick={handleHomeClick}>홈</button>
-      <Link to={`/guide/${guide.userId}/scheduledtours`}>진행 중인 투어</Link>
-      <Link to={`/guide/${guide.userId}/endedtours`}>지난 투어</Link>
-      <Link to={`/guide/${guide.userId}/reviews`}>후기</Link>
+      <GuideDetailMenu
+        userId={guide.userId}
+        handleHomeClick={handleHomeClick}
+      />
     </div>
   );
 };
