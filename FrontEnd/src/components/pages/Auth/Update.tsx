5f317ebca6318b524bd5a01ee52dc4384d585b87
niyamallo <treenicole@naver.com>
2023-08-02 15:21:56 +0900

    Feat : 유저, 가이드 정보 수정페이지 컴포넌트연결

--- FrontEnd/src/components/pages/Auth/Update.tsx
+++ FrontEnd/src/components/pages/Auth/Update.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
 import GeneralUpdate from "./GeneralUpdate";
+import GuideUpdate from "./GuideUpdate";
 
 const Update = () => {
   return (
     <div>
       <h1>업데이트페이지</h1>
-      <GeneralUpdate />
+      {localStorage.getItem("userType") === "GUIDE" ? (
+        <GuideUpdate />
+      ) : (
+        <GeneralUpdate />
+      )}
     </div>
   );
 };
