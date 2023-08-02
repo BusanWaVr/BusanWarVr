6a2f2a7eb63a2f149a936bac001bc19cdb879dc6
정은경 <f1rstf1y9@naver.com>
2023-08-02 16:04:52 +0900

    Fix : 투어 등록할 때 진행 시간을 선택할 수 있도록 수정
    시작일, 종료일의 날짜를 같은 날짜로 입력받고, 시작, 종료 시간을 설정할 수 있도록 수정
    - [GitLab](https://lab.ssafy.com/s09-webmobile1-sub2/S09P12E102/-/issues/44)

---
+++ FrontEnd/src/components/pages/Tour/TourDatePicker.tsx
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import setHours from "date-fns/setHours";
+import setMinutes from "date-fns/setMinutes";
+import { ko } from "date-fns/locale";
+
+const TourDatePicker = ({ setTourData }) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [limitStartMin, setLimitStartMin] = useState(45);
+  const [limitStartHour, setLimitStartHour] = useState(23);
+  const [limitEndMin, setLimitEndMin] = useState(15);
+  const [limitEndHour, setLimitEndHour] = useState(0);
+
+  useEffect(() => {
+    let newLimitEndHour = startDate.getHours();
+    let newLimitEndMin = startDate.getMinutes() + 15;
+    if (newLimitEndMin == 60) {
+      newLimitEndMin -= 60;
+      newLimitEndHour += 1;
+    }
+
+    setLimitEndHour(newLimitEndHour);
+    setLimitEndMin(newLimitEndMin);
+  }, [startDate]);
+
+  useEffect(() => {
+    let newLimitStartHour = endDate.getHours();
+    let newLimitStartMin = endDate.getMinutes() - 15;
+
+    if (newLimitStartMin == -15) {
+      if (newLimitStartHour == 0) {
+        newLimitStartHour = 0;
+        newLimitStartMin = 1;
+      } else {
+        newLimitStartMin += 60;
+        newLimitStartHour -= 1;
+      }
+    }
+    setLimitStartHour(newLimitStartHour);
+    setLimitStartMin(newLimitStartMin);
+  }, [endDate]);
+
+  const handleDate = (date: Date) => {
+    const newStartDate = new Date(startDate);
+    const newEndDate = new Date(endDate);
+
+    newStartDate.setFullYear(date.getFullYear());
+    newStartDate.setMonth(date.getMonth());
+    newStartDate.setDate(date.getDate());
+
+    newEndDate.setFullYear(date.getFullYear());
+    newEndDate.setMonth(date.getMonth());
+    newEndDate.setDate(date.getDate());
+
+    setStartDate(newStartDate);
+    setEndDate(newEndDate);
+    setTourData((prevData) => ({
+      ...prevData,
+      startDate: newStartDate,
+      endDate: newEndDate,
+    }));
+  };
+
+  const handleStartDate = (date: Date) => {
+    const newStartDate = new Date(date);
+    setStartDate(newStartDate);
+    setTourData((prevData) => ({ ...prevData, startDate: newStartDate }));
+
+    let newLimitEndMin = date.getMinutes();
+    let newLimitEndHour = date.getHours();
+
+    if (newLimitEndMin >= 45) {
+      newLimitEndHour += 1;
+      newLimitEndMin = 0;
+    } else {
+      newLimitEndMin = Math.ceil(newLimitEndMin / 15) * 15;
+    }
+
+    setLimitEndHour(newLimitEndHour);
+    setLimitEndMin(newLimitEndMin);
+
+    if (newLimitEndHour === 24 && newLimitEndMin === 0) {
+      setLimitStartHour(0);
+      setLimitStartMin(15);
+    }
+  };
+
+  const handleEndDate = (date: Date) => {
+    const newEndDate = new Date(date);
+    setEndDate(newEndDate);
+    setTourData((prevData) => ({ ...prevData, endDate: newEndDate }));
+  };
+
+  return (
+    <>
+      <DatePicker
+        dateFormat="yyyy/MM/dd"
+        selected={startDate}
+        onChange={handleDate}
+        locale={ko}
+      />
+      <p>시간 선택</p>
+      <span>시작 시간</span>
+      <DatePicker
+        selected={startDate}
+        onChange={handleStartDate}
+        showTimeSelect
+        showTimeSelectOnly
+        timeIntervals={15}
+        timeCaption="Time"
+        dateFormat="h:mm aa"
+        minTime={setHours(setMinutes(startDate, 0), 0)}
+        maxTime={setHours(setMinutes(startDate, limitStartMin), limitStartHour)}
+        locale={ko}
+      />
+      <br />
+      <span>종료 시간</span>
+      <DatePicker
+        selected={endDate}
+        onChange={handleEndDate}
+        showTimeSelect
+        showTimeSelectOnly
+        timeIntervals={15}
+        timeCaption="Time"
+        dateFormat="h:mm aa"
+        minTime={setHours(setMinutes(endDate, limitEndMin), limitEndHour)}
+        maxTime={setHours(setMinutes(endDate, 59), 23)}
+        locale={ko}
+      />
+    </>
+  );
+};
+
+export default TourDatePicker;
