c0d2d3fa12c293b77c1757d53c78533b9ff7049d
김민구 <tkwkalsrn@naver.com>
2023-08-03 11:19:26 +0900

    Feat : 사용자 팔로잉 가이드 리스트 추가 정보 전달
    - Front-End의 요청에 따라 imageUrl과 전체 리뷰 평균 평점을 전달할 수
      있도록 함
    - 생성자를 활용해 일부 코드를 간결화했음

--- BackEnd/src/main/java/com/example/backend/dto/userinfo/UserInfoForGuideReviewsDto.java
+++ BackEnd/src/main/java/com/example/backend/dto/userinfo/UserInfoForGuideReviewsDto.java
@@ -1,5 +1,6 @@
 package com.example.backend.dto.userinfo;
 
+import com.example.backend.model.user.User;
 import lombok.Data;
 
 @Data
@@ -7,5 +8,10 @@ public class UserInfoForGuideReviewsDto {
 
     private Long id;
     private String name;
+
+    public UserInfoForGuideReviewsDto(User user) {
+        this.id = user.getId();
+        this.name = user.getNickname();
+    }
 
 }
