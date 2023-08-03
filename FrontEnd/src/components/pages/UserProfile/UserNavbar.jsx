0aff2faec7a71bd350cd661816c517a6468217e7
unknown <bluemin_@naver.com>
2023-08-03 12:09:58 +0900

    Fix : 이미지 전역에서 왼쪽정렬 되던 현상 수정 이미지 전역에서 왼쪽정렬 되던 현상 수정

--- FrontEnd/src/components/pages/UserProfile/UserNavbar.jsx
+++ FrontEnd/src/components/pages/UserProfile/UserNavbar.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function UserNavbar() {
   return (
     <nav>
       <ul>
-        <li><Link to="/user/:userId/mypage">내 정보</Link></li>
-        <li><Link to="/user/:userId/mypage/tour">내 여행</Link></li>
-        <li><Link to="/user/:userId/mypage/wish">위시리스트</Link></li>
-        <li><Link to="/user/:userId/mypage/following">팔로잉 가이드</Link></li>
+        <li>
+          <Link to="/user/:userId/mypage/">내 정보</Link>
+        </li>
+        <li>
+          <Link to="/user/:userId/mypage/tour">내 여행</Link>
+        </li>
+        <li>
+          <Link to="/user/:userId/mypage/wish">위시리스트</Link>
+        </li>
+        <li>
+          <Link to="/user/:userId/mypage/following">팔로잉 가이드</Link>
+        </li>
       </ul>
     </nav>
   );
