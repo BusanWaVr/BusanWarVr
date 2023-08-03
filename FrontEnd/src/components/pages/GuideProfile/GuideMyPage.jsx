444b3f14d77249ef7c7c002fcbc8082763df356e
niyamallo <treenicole@naver.com>
2023-08-04 00:29:27 +0900

    Feat : 가이드마이페이지 최상위컴포넌트 완성

--- FrontEnd/src/components/pages/GuideProfile/GuideMyPage.jsx
+++ FrontEnd/src/components/pages/GuideProfile/GuideMyPage.jsx
@@ -27,6 +27,7 @@ function GuideMyPage() {
   const { userId } = useSelector((state) => state.userInfo);
   const url = `http://52.79.93.203/guide/guideInfo/${userId}`;
   const [guideInfoData, setGuideInfoData] = useState(null);
+  const [userInfoData, setUserInfoData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -53,10 +54,38 @@ function GuideMyPage() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://52.79.93.203/user/userInfo/${userId}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        if (response.status === 200) {
+          console.log("유저데이터 받았어요");
+          const data = await response.json();
+          setUserInfoData(data.data);
+          console.log("부모에서 넘겨주고 있음", data.data);
+        } else {
+          alert("유저데이터를 받아올 수 없습니다. 잠시 후 다시 시도해 주세요.");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Wrapper>
       <NavbarWrapper>
-        <GuideMini guideInfoData={guideInfoData} />
+        <GuideMini guideInfoData={guideInfoData} userInfoData={userInfoData} />
         <GuideNavbar />
       </NavbarWrapper>
       <OutletWrapper>
