bae91052d2469a467e191a488c9e07294e7d770b
unknown <bluemin_@naver.com>
2023-08-04 17:04:44 +0900

    Feat : 리뷰 디테일 수정 리뷰 디테일 수정

--- FrontEnd/src/components/pages/UserProfile/UserNavbar.jsx
+++ FrontEnd/src/components/pages/UserProfile/UserNavbar.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function UserNavbar() {
+  const { userId } = useParams();
+
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/user/:userId/mypage/">내 정보</Link>
+          <Link to={`/user/${userId}/mypage/`}>내 정보</Link>
         </li>
         <li>
-          <Link to="/user/:userId/mypage/tour">내 여행</Link>
+          <Link to={`/user/${userId}/mypage/tour`}>내 여행</Link>
         </li>
         <li>
-          <Link to="/user/:userId/mypage/wish">위시리스트</Link>
+          <Link to={`/user/${userId}/mypage/wish`}>위시리스트</Link>
         </li>
         <li>
-          <Link to="/user/:userId/mypage/following">팔로잉 가이드</Link>
+          <Link to={`/user/${userId}/mypage/following`}>팔로잉 가이드</Link>
         </li>
         <li>
-          <Link to="/user/:userId/mypage/review">작성한 리뷰</Link>
+          <Link to={`/user/${userId}/mypage/review`}>작성한 리뷰</Link>
         </li>
       </ul>
     </nav>
