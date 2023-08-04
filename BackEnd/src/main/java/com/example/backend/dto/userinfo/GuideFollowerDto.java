88ec784a3d2e2853766ec38b3669cd0a932c88fc
jaeyoung <wodud9515@naver.com>
2023-08-04 12:17:55 +0900

    Feat : 가이드 팔로워 목록 Api 구현
    - 가이드를 팔로우 한 팔로워들의 목록 불러오기
    - 팔로워들의 정보는 아이디, 닉네임, 프로필 이미지만 출력

---
+++ BackEnd/src/main/java/com/example/backend/dto/userinfo/GuideFollowerDto.java
@@ -0,0 +1,22 @@
+package com.example.backend.dto.userinfo;
+
+import com.example.backend.model.follower.Follower;
+import com.example.backend.model.user.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class GuideFollowerDto {
+
+    private Long userId;
+    private String nickname;
+    private String profileImg;
+
+    public GuideFollowerDto(Follower follower){
+        User user = follower.getUser();
+        this.userId = user.getId();
+        this.nickname = user.getNickname();
+        this.profileImg = user.getProfileImg();
+    }
+}
