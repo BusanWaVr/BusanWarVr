84e50bb1176b6849a415663112f835d1838c6070
unknown <bluemin_@naver.com>
2023-08-04 12:22:45 +0900

    Feat : 마이페이지 완성 마이페이지 완성

--- FrontEnd/src/components/pages/UserProfile/UserEndedBoard.jsx
+++ FrontEnd/src/components/pages/UserProfile/UserEndedBoard.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useOutletContext } from "react-router";
+import TourCard from "../../blocks/TourCard";
 
 function UserEndedBoard() {
+  const { userTourData } = useOutletContext();
+
+  if (!userTourData || !userTourData.endedTours) {
+    return <div>Loading...</div>;
+  }
+
+  const userEndedData = userTourData.endedTours;
+
+  useEffect(() => {
+    console.log("종료보드에서 받고 있음", userEndedData);
+  }, [userEndedData]);
+
+  const TourData = userEndedData;
+
   return (
     <div>
       <h1>종료보드</h1>
+      <TourCard TourData={TourData} />
     </div>
   );
 }
