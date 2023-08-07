527fc022aa0c73b5d135ccc8f1d96d5cbf0b7d0f
jh2dang <bluemin_@naver.com>
2023-08-07 23:01:17 +0900

    Feat : 채팅 개설 구현 채팅 개설 구현

--- FrontEnd/src/components/pages/GuideProfile/TestPage.jsx
+++ FrontEnd/src/components/pages/GuideProfile/TestPage.jsx
@@ -1,23 +1,62 @@
-
+import { useState } from "react";
 
 function TestPage() {
-    const startChat
+    const accessToken = localStorage.getItem("accessToken");
 
+    const [tourId, setTourId] = useState("");
+
+    const handleChangeTourId = (e) => {
+        setTourId(e.target.value);
+    }
+
+    const startChat = async () => {
+        try {
+            const requestBody = {
+                tourId: tourId,
+              };
+
+            const response = await fetch("/api/chatroom/start", {
+              method: "POST",
+              headers: {
+                Authorization: accessToken,
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(requestBody),
+            });
+            if (response.status === 200) {
+              const data = await response.json();
+              console.log("요청", requestBody);
+              console.log("응답", data);
+            } else {
+              console.log(data.message);
+              alert(data.message);
+            }
+          } catch (error) {
+            console.error(error);
+          }
+    }
 
 
   return (
     <div>
         <p>채팅 기능 테스트를 위해 임시로 만든 페이지입니다</p>
-        <p>본인이 개설한 tourId를 확인하고 입력해주세요!</p>
+        <p>본인이 개설한 tourId를 확인하고 입력해주세요</p>
 
-        <form onsubmit={startChat}>
+        {/* <form onSubmit={startChat}>
             <input type="text"
                 value={tourId}
                 onChange={handleChangeTourId}
                 placeholder="tourId"
             />
             <button type="submit">채팅 시작</button>
-        </form>
+        </form> */}
+
+        <input type="text" 
+            onChange={handleChangeTourId}
+            placeholder="tourId"
+            value={tourId}
+        />
+        <button type="button" onClick={startChat}>채팅 시작</button>
     </div>
   );
 }
