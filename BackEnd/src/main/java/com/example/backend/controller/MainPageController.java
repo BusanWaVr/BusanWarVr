4b34c1ad0b85e83c43fdcff3e818a0459dcf6790
jaeyoung <wodud9515@naver.com>
2023-08-08 15:13:06 +0900

    Feat : 추천 투어 목록 Api 구현
    - 사용자 회원 가입 시 등록한 카테고리 정보를 가져와 동일한 카테고리를
      가지는 투어를 보여주는 알고리즘으로 추천 투어 목록 Api 구현

---
+++ BackEnd/src/main/java/com/example/backend/controller/MainPageController.java
@@ -0,0 +1,31 @@
+package com.example.backend.controller;
+
+import com.example.backend.dto.Response;
+import com.example.backend.dto.mainPage.TourRecommendDto;
+import com.example.backend.security.UserDetailsImpl;
+import com.example.backend.service.mainPage.MainPageService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MainPageController {
+
+    private final MainPageService mainPageService;
+
+    @GetMapping("/main/recommend")
+    public Response<List<TourRecommendDto>> tourRecommendApi(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PageableDefault(size = 6) Pageable pageable) {
+        List<TourRecommendDto> response = mainPageService.tourRecommend(userDetails.getUser(),
+                pageable);
+
+        return new Response<>("200", "성공적으로 추천 투어 목록을 불러왔습니다.", response);
+    }
+
+}
---
+++ BackEnd/src/main/java/com/example/backend/service/mainPage/MainPageService.java
@@ -0,0 +1,57 @@
+package com.example.backend.service.mainPage;
+
+import com.example.backend.dto.mainPage.TourRecommendDto;
+import com.example.backend.model.tour.Tour;
+import com.example.backend.model.tour.TourRepository;
+import com.example.backend.model.user.User;
+import com.example.backend.model.usercategory.UserCategory;
+import com.example.backend.model.usercategory.UserCategoryRepository;
+import com.example.backend.util.category.CategoryUtil;
+import com.example.backend.util.image.ImageUtil;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MainPageService {
+
+    private final TourRepository tourRepository;
+    private final UserCategoryRepository userCategoryRepository;
+    private final CategoryUtil categoryUtil;
+    private final ImageUtil imageUtil;
+
+    public List<TourRecommendDto> tourRecommend(User user, Pageable pageable) {
+        List<Tour> tours = tourRepository.findByIsEndedFalseOrderByStartDate();
+        List<TourRecommendDto> tourRecommendDtoList = new ArrayList<>();
+        List<UserCategory> userCategoryList = userCategoryRepository.findAllByUser(user);
+
+        for (Tour tour : tours) {
+            Long tourId = tour.getId();
+
+            List<String> tourCategories = new ArrayList<>();
+            categoryUtil.tourCategoryList(tourId, tourCategories);
+
+            if (tour.isEnded() || tour.isCanceled()
+                    || tour.getCurrentMember() >= tour.getMaxMember()) {
+                continue;
+            }
+
+            if (!categoryUtil.isContainCategory(userCategoryList, tourCategories)) {
+                continue;
+            }
+
+            List<String> tourImageUrls = new ArrayList<>();
+            imageUtil.tourImageUrlList(tourId, tourImageUrls);
+
+            tourRecommendDtoList.add(
+                    new TourRecommendDto(tour, tourCategories, tourImageUrls));
+        }
+
+        return tourRecommendDtoList;
+    }
+
+
+}
