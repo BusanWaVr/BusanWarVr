dd0d71ed8164742113ca37708cb777f501b5a6b0
정은경 <f1rstf1y9@naver.com>
2023-08-10 17:05:59 +0900

    Design : 유저 상세 정보 페이지 navbar 스타일 적용
    유저 상세 정보의 좌측 navbar 스타일 적용

---
+++ FrontEnd/src/components/pages/UserProfile/UserNavbar.tsx
@@ -0,0 +1,53 @@
+import { Link, useParams } from "react-router-dom";
+import { Menu } from "antd";
+import {
+  UserOutlined,
+  RocketOutlined,
+  HeartOutlined,
+  UsergroupAddOutlined,
+  FormOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+
+function UserNavbar() {
+  const { userId } = useParams();
+
+  const items: MenuProps["items"] = [
+    {
+      label: <Link to={`/user/${userId}/mypage/`}>내 정보</Link>,
+      key: "1",
+      icon: <UserOutlined />,
+    },
+    {
+      label: <Link to={`/user/${userId}/mypage/tour`}>내 투어 리스트</Link>,
+      key: "2",
+      icon: <RocketOutlined />,
+    },
+    {
+      label: <Link to={`/user/${userId}/mypage/wish`}>위시리스트</Link>,
+      key: "3",
+      icon: <HeartOutlined />,
+    },
+    {
+      label: <Link to={`/user/${userId}/mypage/following`}>팔로잉 가이드</Link>,
+      key: "4",
+      icon: <UsergroupAddOutlined />,
+    },
+    {
+      label: <Link to={`/user/${userId}/mypage/review`}>작성한 리뷰</Link>,
+      key: "5",
+      icon: <FormOutlined />,
+    },
+  ];
+
+  return (
+    <Menu
+      theme="light"
+      defaultSelectedKeys={["1"]}
+      mode="inline"
+      items={items}
+    />
+  );
+}
+
+export default UserNavbar;
