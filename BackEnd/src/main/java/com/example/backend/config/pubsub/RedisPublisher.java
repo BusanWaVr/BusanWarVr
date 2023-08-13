0ea76387d5dc8a9f07bb2b4aa100cf65b48a8a03
wjddudqls96 <wjddudqls96@gmail.com>
2023-08-13 16:16:14 +0900

    Feat : 투표하기 Response 값 변경

--- BackEnd/src/main/java/com/example/backend/config/pubsub/RedisPublisher.java
+++ BackEnd/src/main/java/com/example/backend/config/pubsub/RedisPublisher.java
@@ -14,26 +14,30 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class RedisPublisher {
+
     private final RedisTemplate<String, Object> redisTemplate;
     private final ChannelTopic channelTopic;
     private final SimpMessageSendingOperations messagingTemplate;
 
-    public void chatMessagePublish(ChatMessageResponseDto messageDto){
-        messagingTemplate.convertAndSend("/sub/chat/message/room/" + messageDto.getRoomUid(), messageDto);
+    public void chatMessagePublish(ChatMessageResponseDto messageDto) {
+        messagingTemplate.convertAndSend("/sub/chat/message/room/" + messageDto.getRoomUid(),
+                messageDto);
     }
 
     // 가이드가 투표를 만들면 칼럼명들을 보내준다.
-    public void chatCreateVotePublish(CreateVoteDto.Response response){
-        messagingTemplate.convertAndSend("/sub/chat/vote/create/room/" + response.getRoomUid(), response);
+    public void chatCreateVotePublish(CreateVoteDto.Response response) {
+        messagingTemplate.convertAndSend("/sub/chat/vote/create/room/" + response.getRoomUid(),
+                response);
     }
 
     // 사용자들의 투표내용들을 가이드와 사용자들에게 전부 보내준다.
-    public void chatVotePublish(VoteMessageDto.Request request, User user){
-        messagingTemplate.convertAndSend("/sub/chat/vote/room/" + request.getRoomUid(), request);
+    public void chatVotePublish(VoteMessageDto.Request request, User user) {
+        messagingTemplate.convertAndSend("/sub/chat/vote/room/" + request.getRoomUid(),
+                new VoteMessageDto.Response(request, user));
     }
 
     //가이드가 투표를 종료한다.
-    public void chatVoteEndPublish(EndVoteDto.Request request){
+    public void chatVoteEndPublish(EndVoteDto.Request request) {
         messagingTemplate.convertAndSend("/sub/chat/vote/end/" + request.getRoomUid(), true);
     }
 }
