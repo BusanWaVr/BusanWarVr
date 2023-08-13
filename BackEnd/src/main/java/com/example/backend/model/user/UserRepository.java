a3a094c5f393762abe33bd5efe5d5554a3796de2
wjddudqls96 <wjddudqls96@gmail.com>
2023-08-13 13:39:41 +0900

    Fix : #113

--- BackEnd/src/main/java/com/example/backend/model/user/UserRepository.java
+++ BackEnd/src/main/java/com/example/backend/model/user/UserRepository.java
@@ -15,5 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByNickname(String name);
 
+    List<User> findAllByNicknameContaining(String name);
+
     List<User> findAllByType(AuthType authType);
 }
