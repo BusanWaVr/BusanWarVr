a146518ea9af917cc151c31893df96f8f1ee6390
정은경 <f1rstf1y9@naver.com>
2023-08-14 17:34:28 +0900

    Fix : 라이브 스트리밍 시작 버튼 수정
    라이브 스트리밍 시작 버튼 수정

--- FrontEnd/src/components/blocks/TourStartBtn.jsx
+++ FrontEnd/src/components/blocks/TourStartBtn.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  setTourId,
-  setTourUID,
-} from "../pages/LiveStream/LiveStreamReducer";
-
+import { Button } from "@nextui-org/react";
+import { setTourId, setTourUID } from "../pages/LiveStream/LiveStreamReducer";
 
 // 가이드의 투어시작하기 버튼 컴포넌트
 function TourStartBtn({ Tour }) {
@@ -14,14 +11,12 @@ function TourStartBtn({ Tour }) {
 
   // 리듀서에 저장
   useEffect(() => {
-    dispatch(setTourUID(Tour.uid))
-    dispatch(setTourId(Tour.tourId))
+    dispatch(setTourUID(Tour.uid));
+    dispatch(setTourId(Tour.tourId));
   }, [dispatch]);
 
   const handleStartClick = () => {
-    navigate("../../../../livestream", {
-      state: { tourUID: tourUID, tourId: tourId },
-    });
+    navigate("../../../../livestream");
   };
 
   return (
