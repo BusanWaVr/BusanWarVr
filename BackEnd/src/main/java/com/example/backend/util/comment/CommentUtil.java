e35bc8932ab0aedcebf9bc7b6693130149db8c6d
jaeyoung <wodud9515@naver.com>
2023-08-15 03:13:05 +0900

    Feat : 댓글 목록 보기 코드 수정
    - 출력 결과에 투어 참여자인지 유무, 부모 댓글 아이디, 현재 댓글 아이디 데이터 추가

--- BackEnd/src/main/java/com/example/backend/util/comment/CommentUtil.java
+++ BackEnd/src/main/java/com/example/backend/util/comment/CommentUtil.java
@@ -4,6 +4,10 @@ import com.example.backend.dto.comment.CommentDto;
 import com.example.backend.dto.comment.ReCommentDto;
 import com.example.backend.model.comment.Comment;
 import com.example.backend.model.comment.CommentRepository;
+import com.example.backend.model.joiner.Joiner;
+import com.example.backend.model.joiner.JoinerRepository;
+import com.example.backend.model.tour.Tour;
+import com.example.backend.model.tour.TourRepository;
 import com.example.backend.model.user.User;
 import com.example.backend.model.user.UserRepository;
 import java.util.ArrayList;
@@ -17,26 +21,40 @@ public class CommentUtil {
 
     private final CommentRepository commentRepository;
     private final UserRepository userRepository;
+    private final TourRepository tourRepository;
+    private final JoinerRepository joinerRepository;
 
     public void commentDtoList(Long tourId, List<CommentDto> commentDtos) {
+        Tour tour = tourRepository.findById(tourId).get();
         List<Comment> commentList = commentRepository.findAllByTourId(tourId);
         for (Comment comment : commentList) {
-            if (comment.getParentId() == null) {
-                User user = userRepository.findById(comment.getUserId()).get();
-
+            User user = userRepository.findById(comment.getUserId()).get();
+            List<Joiner> joinerList = joinerRepository.findAllByTourId(tourId);
+            if (comment.getParentId() == null && joinerList.contains(user)) {
                 List<ReCommentDto> reCommentList = new ArrayList<>();
                 reCommentDtoList(comment, reCommentList);
-                CommentDto rootComment = new CommentDto(user, comment, reCommentList);
+                CommentDto rootComment = new CommentDto(user, comment, true, reCommentList);
                 commentDtos.add(rootComment);
+                continue;
             }
+            List<ReCommentDto> reCommentList = new ArrayList<>();
+            reCommentDtoList(comment, reCommentList);
+            CommentDto rootComment = new CommentDto(user, comment, false, reCommentList);
+            commentDtos.add(rootComment);
         }
     }
 
     public void reCommentDtoList(Comment comment, List<ReCommentDto> reCommentList){
         List<Comment> reComments = commentRepository.findAllByParentId(comment.getId());
         for (Comment reComment : reComments){
+            List<Joiner> joinerList = joinerRepository.findAllByTourId(reComment.getTourId());
             User user = userRepository.findById(reComment.getUserId()).get();
-            ReCommentDto reCommentDto = new ReCommentDto(user, reComment);
+            if(joinerList.contains(user)){
+                ReCommentDto reCommentDto = new ReCommentDto(user, true, reComment);
+                reCommentList.add(reCommentDto);
+                continue;
+            }
+            ReCommentDto reCommentDto = new ReCommentDto(user, false, reComment);
             reCommentList.add(reCommentDto);
         }
     }
