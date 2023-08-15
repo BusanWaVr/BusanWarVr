192331ef90a038b1ebb15c025605299b5f649b27
정은경 <f1rstf1y9@naver.com>
2023-08-16 02:29:14 +0900

    Design : 메이트 등록 페이지 스타일 적용
    메이트 등록 페이지 스타일 적용

--- FrontEnd/src/components/pages/Mate/MateWrite.jsx
+++ FrontEnd/src/components/pages/Mate/MateWrite.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import Responsive from "../../common/Responsive";
 import { useParams } from "react-router-dom";
-import Editor from "../../blocks/Editor";
 import styled from "styled-components";
+import { Input, Button, Avatar } from "antd";
+import Editor from "../../blocks/Editor";
+import { toast } from "react-toastify";
+import { EditOutlined } from "@ant-design/icons";
 
 const StyledWritePage = styled.div`
   width: 60%;
@@ -13,16 +16,45 @@ const MateWrite = () => {
   const { tourId } = useParams();
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [tourData, setTourData] = useState(null);
 
   const accessToken = localStorage.getItem("accessToken");
+
+  useEffect(() => {
+    const fetchTourData = async () => {
+      try {
+        const response = await fetch(
+          `https://busanwavrserver.store/tour/${tourId}`
+        );
+        if (response.status === 200) {
+          const res = await response.json();
+          setTourData(res.data);
+        } else {
+          toast.error("해당 투어가 존재하지 않습니다.");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchTourData();
+  }, [tourId]);
 
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
   };
 
   const handleSubmit = async (e) => {
-    console.log("슈웃");
     e.preventDefault();
+
+    if (!title) {
+      toast.warning("제목을 입력해주세요!");
+      return;
+    }
+    if (!content) {
+      toast.warning("내용을 입력해주세요!");
+      return;
+    }
 
     if (tourId && title && content) {
       try {
@@ -45,12 +77,10 @@ const MateWrite = () => {
 
         if (data.code === "200") {
           const newMateId = data.data.mateId;
-          alert(data.message);
+          toast.success(data.message);
           window.location.href = `/matedetail/${newMateId}`;
         } else {
-          // 에러
-          console.log(data.message);
-          alert("죄송합니다. 잠시후 다시 시도해 주세요.");
+          toast.error("죄송합니다. 잠시후 다시 시도해 주세요.");
         }
       } catch (error) {
         console.error(error);
@@ -58,37 +88,55 @@ const MateWrite = () => {
     }
   };
 
-  return (
-    // <Responsive>
-    <StyledWritePage>
+  return tourData ? (
+    <div className="mx-4 sm:mx-24 lg:mx-48 2xl:mx-96 my-6">
       <div>
-        <h1>메이트 모집 글쓰기 페이지</h1>
         <br />
         <form>
-          {/* 임시 */}
-          <label htmlFor="tourId">투어 id :　</label>
-          <input type="text" id="tourId" value={tourId} disabled />
+          <div className="w-1/3 mx-auto mt-6 flex flex-col items-center gap-2">
+            <img
+              src={tourData.tourImgs[0]}
+              alt=""
+              className="w-40 h-40 rounded-full object-cover"
+            />
+            <p className="font-bold text-lg">{tourData.title}</p>
+            <div className="flex items-center justify-center gap-1 text-sm">
+              <Avatar src={tourData.profileImg} alt="" />
+              <p>{tourData.nickname}</p>
+            </div>
+          </div>
           <br />
           <br />
-          <label htmlFor="title">제목 :　</label>
-          <input
+          <Input
             type="text"
-            id="title"
+            placeholder="게시글의 제목을 입력해주세요."
             value={title}
             onChange={handleTitleChange}
-            placeholder="제목을 입력해 주세요."
+            style={{ height: "45px" }}
           />
           <br />
           <br />
-          <Editor value={content} onChange={setContent} />
+          <Editor value={content} onChange={setContent} customHeight="300px" />
         </form>
       </div>
       <br />
-      <button type="submit" onClick={handleSubmit}>
-        등록
-      </button>
-    </StyledWritePage>
-    // </Responsive>
+      <Button
+        type="primary"
+        onClick={handleSubmit}
+        icon={<EditOutlined />}
+        style={{
+          width: "100%",
+          height: "45px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        메이트 모집 등록
+      </Button>
+    </div>
+  ) : (
+    <div>로딩 중 ...</div>
   );
 };
 
