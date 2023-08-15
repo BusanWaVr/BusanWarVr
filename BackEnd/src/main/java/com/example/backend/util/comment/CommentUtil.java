bc4a2392fc7173b071dd6d5b3503914317a96df8
jaeyoung <wodud9515@naver.com>
2023-08-16 00:22:43 +0900

    Feat : 댓글 Util 수정

--- BackEnd/src/main/java/com/example/backend/util/comment/CommentUtil.java
+++ BackEnd/src/main/java/com/example/backend/util/comment/CommentUtil.java
@@ -25,10 +25,12 @@ public class CommentUtil {
 
     public void commentDtoList(Long tourId, List<CommentDto> commentDtos) {
         List<Comment> commentList = commentRepository.findAllByTourId(tourId);
+
         for (Comment comment : commentList) {
             User user = userRepository.findById(comment.getUserId()).get();
             List<Joiner> joinerList = joinerRepository.findAllByTourId(tourId);
             boolean isExist = joinerUtil.isExistJoinerList(user, joinerList);
+
             if (comment.getParentId() == null) {
                 List<ReCommentDto> reCommentList = new ArrayList<>();
                 reCommentDtoList(comment, reCommentList);
@@ -36,6 +38,7 @@ public class CommentUtil {
                 commentDtos.add(rootComment);
                 continue;
             }
+
             List<ReCommentDto> reCommentList = new ArrayList<>();
             reCommentDtoList(comment, reCommentList);
             CommentDto rootComment = new CommentDto(user, comment, isExist, reCommentList);
