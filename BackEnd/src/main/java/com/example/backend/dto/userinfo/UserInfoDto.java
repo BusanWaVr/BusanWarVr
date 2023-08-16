e2fb7c7d0cb4af99f01bd8ffdd613c3694f5ac2b
jybin96 <wjddudqls96@gmail.com>
2023-08-16 10:04:33 +0900

    Feat : 프런트 요청으로 review 부분 Response 변경

--- BackEnd/src/main/java/com/example/backend/dto/userinfo/UserInfoDto.java
+++ BackEnd/src/main/java/com/example/backend/dto/userinfo/UserInfoDto.java
@@ -1,15 +1,18 @@
 package com.example.backend.dto.userinfo;
 
+import com.example.backend.dto.review.ReviewUserInfoDto;
 import com.example.backend.model.enums.AuthType;
 import com.example.backend.model.review.Review;
 import com.example.backend.model.user.User;
 import java.util.List;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 public class UserInfoDto {
 
     @Data
+    @NoArgsConstructor
     public static class Response {
 
         private Long userId;
@@ -20,9 +23,9 @@ public class UserInfoDto {
         private AuthType type;
         private String introduction;
         private int followingNum;
-        private List<Review> reviews;
+        private List<ReviewUserInfoDto> reviews;
 
-        public Response(User user, List<String> categories, int followingNum, List<Review> reviews) {
+        public Response(User user, List<String> categories, int followingNum, List<ReviewUserInfoDto> reviews) {
             this.userId = user.getId();
             this.email = user.getEmail();
             this.nickname = user.getNickname();
@@ -34,5 +37,4 @@ public class UserInfoDto {
             this.reviews = reviews;
         }
     }
-
 }
