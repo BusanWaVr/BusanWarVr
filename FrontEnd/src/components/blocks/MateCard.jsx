cfd237feb7909f6654874f6daa3bae26e14c819a
정은경 <f1rstf1y9@naver.com>
2023-08-16 10:34:36 +0900

    Design : 메이트 목록 페이지 스타일 적용
    메이트 목록 페이지 스타일 적용

--- FrontEnd/src/components/blocks/MateCard.jsx
+++ FrontEnd/src/components/blocks/MateCard.jsx
@@ -1,27 +1,63 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { useI18n } from "../../hooks/useI18n"
+import { useI18n } from "../../hooks/useI18n";
+import NotPresentMember from "../../assets/boogie_notPresent.jpg";
+import PresentMember from "../../assets/boogie_present.jpg";
 
 const CardContainer = styled.div`
-  margin: 50px;
+  height: 150px;
+  border-radius: 15px;
+  border: 1px solid #1983ff;
 `;
 
 function MateCard({ mateData }) {
-  const t = useI18n()
+  const t = useI18n();
   return (
     <>
-      {mateData.map((mate) => (
-        <CardContainer key={mate.mateId}>
-          <h3>
-            <Link to={`/matedetail/${mate.mateId}`}>{mate.title}</Link>
-          </h3>
-          <div dangerouslySetInnerHTML={{ __html: mate.content }} />
-          <p>
-          {t(`참가인원`)}: {mate.joinMember}/{mate.maxMember}
-          </p>
-        </CardContainer>
-      ))}
+      <div className="w-4/5 gap-6 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 md:px-8 px-6 mx-auto my-6">
+        {mateData.map((mate) => (
+          <CardContainer
+            key={mate.mateId}
+            className="flex flex-col justify-center bg-sky-100 text-left p-6 gap-2"
+          >
+            <p className="text-xs font-semibold">
+              {mate.joinMember} / {mate.maxMember}
+            </p>
+            <h3>
+              <Link to={`/matedetail/${mate.mateId}`}>{mate.title}</Link>
+            </h3>
+            <div className="flex gap-2 w-full">
+              {Array.from(
+                {
+                  length: mate.joinMember,
+                },
+                (_, index) => (
+                  <img
+                    className="w-9 rounded-xl opacity-80"
+                    key={index}
+                    src={PresentMember}
+                    alt="프로필 이미지"
+                  />
+                )
+              )}
+              {Array.from(
+                {
+                  length: mate.maxMember - mate.joinMember,
+                },
+                (_, index) => (
+                  <img
+                    className="w-9 rounded-xl opacity-50"
+                    key={index + mate.joinMember}
+                    src={NotPresentMember}
+                    alt="프로필 이미지"
+                  />
+                )
+              )}
+            </div>
+          </CardContainer>
+        ))}
+      </div>
     </>
   );
 }
