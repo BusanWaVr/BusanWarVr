55d5582b848d28fb43585b89ca16a828b048dd2f
jaeyoung <wodud9515@naver.com>
2023-08-16 11:50:18 +0900

    Feat : 메이트 모집 목록 출력 데이터 변경
    - 투어 이미지, 투어 제목 데이터 추가

--- BackEnd/src/main/java/com/example/backend/dto/mate/MateInfoForListDto.java
+++ BackEnd/src/main/java/com/example/backend/dto/mate/MateInfoForListDto.java
@@ -1,6 +1,7 @@
 package com.example.backend.dto.mate;
 
 import com.example.backend.model.mate.Mate;
+import com.example.backend.model.tour.Tour;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,15 +11,19 @@ public class MateInfoForListDto {
 
     private Long mateId;
     private Long tourId;
+    private String tourTitle;
+    private String tourImage;
     private String title;
     private String content;
     private int minMember;
     private int maxMember;
     private int joinMember;
 
-    public MateInfoForListDto(Mate mate) {
+    public MateInfoForListDto(Mate mate, Tour tour, String tourImage) {
         this.mateId = mate.getId();
         this.tourId = mate.getTourId();
+        this.tourTitle = tour.getTitle();
+        this.tourImage = tourImage;
         this.title = mate.getTitle();
         this.content = mate.getContent();
         this.minMember = mate.getMinMember();
